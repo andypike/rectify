d48cf30a8868fd7c6cb0edb0f156c229ccce4265
David Rodríguez <deivid.rodriguez@riseup.net>
2018-03-20 17:14:57 -0300

    Fix nested indexed arrays support
    Follow up to 862d4dfd3b56d30b95a05f25bb9255a37cbdb1da.

--- spec/fixtures/forms/contact_form.rb
+++ spec/fixtures/forms/contact_form.rb
@@ -1,6 +1,9 @@
+require_relative "phone_form"
+
 class ContactForm < Rectify::Form
   attribute :name,   String
   attribute :number, String
+  attribute :phones, Array[PhoneForm]
 
   validates :name, :presence => true
 end
